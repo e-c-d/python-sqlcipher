74b3987bdf5e74f514a8253767c2ca504bc5637f
Eduard Christian Dumitrescu <eduard.c.dumitrescu@gmail.com>
2021-07-31 03:49:42 -0700

    oops12

--- azure-pipelines.yml.py
+++ azure-pipelines.yml.py
@@ -48,7 +48,7 @@ def define_conda_jobs():
 def main():
     jobs = [
         odict(
-            template="azure-pipelines/get_sources.yml",
+            template="azure-pipelines/get_sources_job.yml",
         ),
     ]
     jobs.extend(define_conda_jobs())
